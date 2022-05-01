ac870cc18ab65dce0f4f7bd97ca029039f3a0d55
Ondrej Svoboda <admin@svoby.eu>
2022-05-01 19:39:39 +0200

    scripts

---
+++ directory-test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+jump_directory=/home/svoby
+
+if [ -d $jump_directory ]
+then
+	echo "The $jump_directory exists"
+	cd $jump_directory
+	ls
+else
+	echo "The $jump_directory does not exist"
+fi
+
---
+++ floating_point_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+value1=5.5555
+
+echo "The test value is $value1"
+
+if [ $value -gt 5 ]
+then
+	echo "The test value $value1 is greater than 5"
+fi
+
