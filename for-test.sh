45de3e9b357f21a2c7cf12bd50aee2876b2d4ee8
Ondrej Svoboda <admin@svoby.eu>
2022-05-02 13:52:13 +0200

    loops

---
+++ for-test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for var in Alabama Alaska Arizona Arkansas California Colorado
+do
+	echo "The next state is $var"
+done
+
---
+++ for-test3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for var in "Nevada" "New Hampshire" "New Mexico" "New York"
+do
+	echo "Now going to $var"
+done
+
