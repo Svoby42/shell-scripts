53d59c785827a6204bb9941015f28d9a7b84c42d
Ondrej Svoboda <admin@svoby.eu>
2022-05-02 09:27:53 +0200

    file checks

---
+++ file-test2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+pwfile=/etc/shadow
+
+if [ -f $pwfile ]
+then
+	if [ -r $pwfile ]
+	then
+		tail $pwfile
+	else
+		echo "Sorry, you are unable to read the $pwfile file"
+	fi
+else
+	echo "Sorry, the file $file does not exist"
+fi
+
