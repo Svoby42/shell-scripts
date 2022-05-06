edc537f7abd5a0ccab80e392d4e5e80c12edf18c
Ondrej Svoboda <admin@svoby.eu>
2022-05-06 13:36:48 +0200

    number guessing game

---
+++ guess-a-number.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+num=$RANDOM
+
+while true
+do
+	read -p "Guess a number " answer
+	echo "Your answer: $answer"
+	if [ $answer -lt $num  ]
+	then
+		echo "Your number is lesser than my number."
+	elif [ $answer -gt $num ]
+	then
+		echo "Your number is greater than my number."
+	else
+		echo "That is correct! Congratulations."
+		break
+	fi
+done
+
